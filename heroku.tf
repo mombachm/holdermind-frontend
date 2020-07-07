cf530c7ec744cf2a9c0fb900c50e50f8d2e2cdf7
Matheus Mombach <matheus.mombach@sap.com>
2020-07-07 03:12:08 -0300

    update terraform file with heroku deployment

--- heroku.tf
+++ heroku.tf
@@ -7,19 +7,13 @@ resource "heroku_app" "holdermind-frontend" {
   region = "us"
 }
 
-data "github_release" "latest-release" {
-    repository  = "example-repository"
-    owner       = "example-owner"
-    retrieve_by = "latest"
-}
-
 resource "heroku_build" "holdermind-frontend" {
   app = heroku_app.holdermind-frontend.id
 
   buildpacks = ["https://github.com/mars/create-react-app-buildpack.git"]
 
   source = {
-    url = "https://github.com/mombachm/holdermind/archive/1.0.tar.gz"
+    url = "https://github.com/mombachm/holdermind-frontend/archive/master.tar.gz"
     version = "1.0"
   }
 }
